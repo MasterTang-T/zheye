0859a24740eee2e8e8793127859dac182765da8f
MasterTang <1286545749@qq.com>
2021-01-20 21:48:53 +0800

    代码规范

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,53 @@
 <template>
-  <div class="container">
+<div class="container">
     <column-list :list="list"></column-list>
-  </div>
+</div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from "vue";
+import {
+    defineComponent,
+    reactive
+} from "vue";
 import "bootstrap/dist/css/bootstrap.min.css";
-import ColumnList, { ColumnProps } from "./components/ColumnList.vue";
-const testData: ColumnProps[] = [
-  {
-    id: 1,
-    title: "test1的专栏",
-    description: "这是一个test1的专栏",
-    // avatar: "xxx.jpg",
-  },
-  {
-    id: 2,
-    title: "test2的专栏",
-    description: "这是一个test2的专栏",
-    // avatar: "xxx.jpg",
-  },
-  {
-    id: 3,
-    title: "test2的专栏",
-    description: "这是一个test2的专栏",
-    // avatar: "xxx.jpg",
-  },
-  {
-    id: 4,
-    title: "test2的专栏",
-    description: "这是一个test2的专栏",
-    // avatar: "xxx.jpg",
-  },
+import ColumnList, {
+    ColumnProps
+} from "./components/ColumnList.vue";
+const testData: ColumnProps[] = [{
+        id: 1,
+        title: "test1的专栏",
+        description: "这是一个test1的专栏",
+        // avatar: "xxx.jpg",
+    },
+    {
+        id: 2,
+        title: "test2的专栏",
+        description: "这是一个test2的专栏",
+        // avatar: "xxx.jpg",
+    },
+    {
+        id: 3,
+        title: "test2的专栏",
+        description: "这是一个test2的专栏",
+        // avatar: "xxx.jpg",
+    },
+    {
+        id: 4,
+        title: "test2的专栏",
+        description: "这是一个test2的专栏",
+        // avatar: "xxx.jpg",
+    },
 ];
 export default defineComponent({
-  name: "App",
-  components: {
-    ColumnList,
-  },
-  setup() {
-    return {
-      list: testData,
-    };
-  },
+    name: "App",
+    components: {
+        ColumnList,
+    },
+    setup() {
+        return {
+            list: testData,
+        };
+    },
 });
 </script>
 
--- src/components/ColumnList.vue
+++ src/components/ColumnList.vue
@@ -1,51 +1,51 @@
 <template>
-  <div class="row mt-3">
+<div class="row mt-3">
     <div v-for="column in columnList" :key="column.id" class="col-4 mb-3">
-      <div class="card h-100 shadow-sm" >
-        <div class="card-body text-center">
-          <img
-            :src="column.avatar"
-            :alt="column.title"
-            class="rounded-circle border border-light w-25 my-3"
-          />
-          <h5 class="card-title">{{ column.title }}</h5>
-          <p class="card-text">{{ column.description }}</p>
-          <a href="#" class="btn btn-primary">进入专栏</a>
+        <div class="card h-100 shadow-sm">
+            <div class="card-body text-center">
+                <img :src="column.avatar" :alt="column.title" class="rounded-circle border border-light w-25 my-3" />
+                <h5 class="card-title">{{ column.title }}</h5>1
+                <p class="card-text">{{ column.description }}</p>
+                <a href="#" class="btn btn-primary">进入专栏</a>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
+</div>
 </template>
 
-<script lang='ts'>
-import { computed, defineComponent, PropType } from "vue";
+<script lang="ts">
+import {
+    computed,
+    defineComponent,
+    PropType
+} from "vue";
 export interface ColumnProps {
-  id: number;
-  title: string;
-  avatar?: string;
-  description: string;
+    id: number;
+    title: string;
+    avatar?: string;
+    description: string;
 }
 export default defineComponent({
-  name: "ColumnList",
-  props: {
-    list: {
-      type: Array as PropType<ColumnProps[]>,
-      required: true,
+    name: "ColumnList",
+    props: {
+        list: {
+            type: Array as PropType <ColumnProps[]> ,
+            required: true,
+        },
     },
-  },
-  setup(props) {
-    const columnList = computed(() => {
-      return props.list.map((column) => {
-        if (!column.avatar) {
-          column.avatar = require("@/assets/avatar.jpg");
-        }
-        return column;
-      });
-    });
-    return {
-      columnList,
-    };
-  },
+    setup(props) {
+        const columnList = computed(() => {
+            return props.list.map((column) => {
+                if (!column.avatar) {
+                    column.avatar = require("@/assets/avatar.jpg");
+                }
+                return column;
+            });
+        });
+        return {
+            columnList,
+        };
+    },
 });
 </script>
 
